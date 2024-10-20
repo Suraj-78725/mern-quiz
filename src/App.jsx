252689a6b9c35d88e272a403883e12abebc80463
Vighnesh Pawar <vighneshpawar004@gmail.com>
2024-10-20 22:41:41 +0530

    20-10-24 started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='font-bold text-center text-3xl'>Quiz Application starter file</div>
+    </>
+  )
+}
+
+export default App
